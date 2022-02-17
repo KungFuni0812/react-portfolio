6d2659b5027bada2ae5254c4ff93daf5302b0839
KungFuni0812 <kainichen407@gmail.com>
2022-02-16 22:48:43 -0500

    initial commit

---
+++ src/pages/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Main = () => {
+    return (
+        <main>
+        <div className="page-header">
+            <h1>About Me</h1>
+        </div>
+        <div>Hello, My name is Kaini Feller, I am a full-stack developer that specifialized in //list of developing langauage//. </div>
+        </main>
+    );
+}
+
+export default Main;
